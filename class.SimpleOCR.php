511bb479630265215e5ac9ed84de7f87ed7beb25
grid <gridsane@gmail.com>
2012-08-14 18:51:49 +0600

    Реорганизация базового функционала, вроде красиво, но в ущерб скорости

--- class.SimpleOCR.php
+++ class.SimpleOCR.php
@@ -26,8 +26,6 @@ class SimpleOCR
 {
     public $font;
     private $text;
-    private $tmpImage = "simpleocr.tmp";
-
     /**
      * Initialize class with font
      * @param  mixed $font array or path to file returns array
@@ -41,48 +39,103 @@ class SimpleOCR
         }
     }
 
-    public function execute ($pathToImage)
+    private function imageSharp (&$imagePath)
     {
-        $dir = $_SERVER["DOCUMENT_ROOT"]."testimg/";
-        $tmpImage = uniqid().$this->tmpImage;
-        system("convert {$dir}{$pathToImage} -threshold 70% -trim {$dir}{$tmpImage}", $res);
-        var_dump($res);
-        $image = imagecreatefrompng($dir.$tmpImage);
+        $tempImage = tempnam(sys_get_temp_dir(), 'imageocr');
+        $code = 0;
+        system("convert {$imagePath} -threshold 70% -trim {$tempImage}", $code);
+        // TODO: exception if code != 0
+        return $tempImage;
+    }
+
+    private function imageToArray (&$imagePath)
+    {
+        $image = imagecreatefrompng($imagePath);
+        $imageArray = array(array());
         $width = imagesx($image);
         $height = imagesy($image);
-
-        $characters = array();
-        $curChar = array();
-
-        $procents = 0;
-
         for ($x = 0; $x < $width; $x++) {
-            $vLine = true;
-            $curColumn = array();
             for ($y = 0; $y < $height; $y++) {
-                $color = imagecolorat($image, $x, $y);
-                if ($color == 0) {
-                    $vLine = false;
-                }
-                $curColumn[] = $color;
+                $imageArray[$x][$y] = (int)imagecolorat($image, $x, $y); 
             }
-            if (!$vLine || ($x+1) == $width) {
-                $curChar[] = $curColumn;
-                $curColumn = array();
-            }
-            if ($vLine || ($x+1) == $width) {
+        }
+        return $imageArray;
+    }
+ 
+    private function explodeLines (&$imageArray) {
+        // TODO: реализация
+        return array($imageArray);
+    }
 
-                if (!empty($curChar)) {
-                    $characters[] = $curChar;
-                    $result = $this->ocr($this->getPattern($curChar));
-                    echo "<pre>"; print_r($this->getPattern($curChar)); echo "</pre>";
-                    $procents += $result[1];
-                    $curChar = array();
-                    $sc = 0;
+    private function explodePattern (&$lineArray) {
+        // TODO: считать количество разделителей
+        // и если нужно ставить пробелы
+        $chars = array();
+        $width = count($lineArray);
+        $height = count($lineArray[0]);
+        $currentChar = array();
+        for ($x = 0; $x < $width; $x++) {
+            $isVertLine = true;
+            for($y = 0; $y < $height; $y++) {
+                if($lineArray[$x][$y] == 0) {
+                    $isVertLine = false;
+                    break;
+                }
+            }
+            if($isVertLine) {
+                if(count($currentChar) > 0) {
+                    $chars[] = $this->getPattern($currentChar);
+                }
+                $currentChar = array();
+            } else {
+                $currentChar[] = $lineArray[$x];
+            }
+        }
+        return $chars;
+    }
+
+    private function patternDiff (&$pattern1, &$pattern2)
+    {
+        $result = 0;
+        for ($i = 0; $i < 4; $i++) {
+            if (isset($pattern2[$i])) {
+                $result += abs($pattern1[$i] - $pattern2[$i]);
+            }
+        }
+        return $result;
+    }
+
+    private function recognize (&$targetPattern)
+    {
+        $minDiff = -1;
+        $result = '';
+        foreach ($this->font as $char => $pattern) {
+            $diff = $this->patternDiff($targetPattern, $pattern);
+            if($minDiff < 0 || $minDiff > $diff) {
+                $minDiff = $diff;
+                $result = $char;
+                if($diff == 0) {
+                    break;
                 }
             }
         }
-        unlink($dir.$tmpImage);
+        return $result;
+    }
+
+    public function execute ($imagePath)
+    {
+        $image = $this->imageSharp($imagePath);
+        $imageArray = $this->imageToArray($image);
+        $text = "";
+        $lines = $this->explodeLines($imageArray);
+        foreach($lines as $line) {
+            $chars = $this->explodePattern($line);
+            foreach($chars as $char) {
+                $text .= $this->recognize($char);
+            }
+            $text .= "\n";
+        }
+        return $text;
     }
 
 
@@ -115,38 +168,5 @@ class SimpleOCR
             }
         }
         return $conf;
-    }
-
-    private function diff (&$array1, &$array2)
-    {
-        $l = count($array1);
-        $result = 0;
-        for ($i = 0; $i < $l; $i++) {
-            if (isset($array2[$i])) {
-                $result += abs((int)$array1[$i] - (int)$array2[$i]);
-            }
-        }
-        return $result;
-    }
-
-    private function ocr (&$input)
-    {
-        $diffs = array();
-        $minDiff = 1000;
-        $result = 0;
-        foreach ($this->font as $NUM => $conf) {
-            $diff = 0;
-            $diff = $this->diff($input, $conf);
-            $diffs[$NUM] = $diff;
-            if ($diff <= $minDiff) {
-                $result = $NUM;
-                $minDiff = $diff;
-            }
-            if ($minDiff == 0) {
-                break;
-            }
-        }
-        echo "<td><b style = 'font-size:1em'>$result</b><br/><font style = 'font-size:0.7em'>".(100 - 0.7*$minDiff)."%</font></td>";
-        return array($result, (100 - 0.7*$minDiff));
     }
 }
--- index.php
+++ index.php
@@ -4,7 +4,7 @@ echo "<pre>";
 require_once("class.SimpleOCR.php");
 
 $ocr = new SimpleOCR("avito.font");
-$text = $ocr->execute("e9721dbec6d15a3146f6cbc95fcb54ea.png");
+$text = $ocr->execute($_SERVER["DOCUMENT_ROOT"]."testimg/e9721dbec6d15a3146f6cbc95fcb54ea.png");
 
 print_r($text);
 echo "</pre>";
